56da8abdd1a14ee6af0400d62762af3db24885fb
KOSMOSq <maks2014216@gmail.com>
2023-07-23 20:09:46 +0300

    added firebase, no redux functionality with firebase

--- src/components/ContactsList.tsx
+++ src/components/ContactsList.tsx
@@ -1,20 +1,40 @@
+import { onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { removeContact } from "../store/features/contactsSlice";
+import { contactsCollection, deleteContactFirebase } from "../lib/controller";
+import { IContacts, removeContact } from "../store/features/contactsSlice";
 import { useAppDispatch, useAppSelector } from "../store/store";
 
 const ContactsList: React.FC = () => {
     const contacts = useAppSelector(state => state.contacts.contacts);
+    const [contact, setContact] = useState<IContacts[]>([]);
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
+    useEffect(() => {
+        onSnapshot(contactsCollection, snapshot => {
+            setContact(
+                snapshot.docs.map(doc => {
+                    const data = doc.data();
+                    return {
+                        id: data.id,
+                        name: data.name,
+                        mail: data.mail
+                    };
+                })
+            );
+        });
+    }, []);
+
     return (
         <>
             <h1>My Address Book</h1>
-            {contacts.map(contact => (
+            {contact.map(contact => (
                 <div key={contact.id}>
                     <h3>Name: {contact.name}</h3>
                     <h4>Mail: {contact.mail}</h4>
                     <button
                         onClick={() => {
+                            deleteContactFirebase(contact.id);
                             dispatch(removeContact(contact.id));
                         }}
                     >
