d5a18bf24d813dd0413a694a3ef75d53cae4995e
KOSMOSq <maks2014216@gmail.com>
2023-08-30 00:54:03 +0300

    added toast notification

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { router } from "./components/Router/Router";
 import "./global.css";
 import { store } from "./store/store";
@@ -13,6 +15,19 @@ root.render(
     <React.StrictMode>
         <Provider store={store}>
             <RouterProvider router={router} />
+            <ToastContainer
+                position="bottom-left"
+                autoClose={2000}
+                limit={10}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </Provider>
     </React.StrictMode>
 );
